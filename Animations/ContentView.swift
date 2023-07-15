098f7cc1fd89d162da6ee8bfd61a79a61f2b6bcc
Kitwana Akil <kakil@me.com>
2023-07-15 15:53:46 -0400

    Completed building custom transitions using ViewModifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,22 +7,47 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var isShowingRed = false
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 0.25
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
             
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3 )
-        .animation(.default, value: animationAmount)
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 

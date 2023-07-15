8bd6b714b65221968f4d9480c37c2c9f2af2b2d7
Kitwana Akil <kakil@me.com>
2023-07-15 15:33:42 -0400

    Completed animating gestures tutorial.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,20 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
+    
+    let letters = Array("Hello, SwiftUI")
+
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 0.25
-            
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount
+                    )
+            }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3 )
-        .animation(.default, value: animationAmount)
+        .gesture(
+            DragGesture()
+                .onChanged {
+                    dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged {
+//                        dragAmount = $0.translation
+//                    }
+//                    .onEnded { _ in
+//                        withAnimation {
+//                            dragAmount = .zero
+//                        }
+//                    }
+//            )
+//            //.animation(.spring(), value: dragAmount)
     }
 }
 

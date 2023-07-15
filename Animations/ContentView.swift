11553227fd4866a2a29a866edbbd8765fd6b966a
Kitwana Akil <kakil@me.com>
2023-07-15 12:40:13 -0400

    Animations binding tutorial complete.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -12,17 +12,27 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 0.25
+        
+        print(animationAmount)
+        
+        return VStack {
+            //Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
             
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3 )
-        .animation(.default, value: animationAmount)
     }
 }
 

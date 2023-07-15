e47bf65d57b3a5060581d210dd24aa75153594c7
Kitwana Akil <kakil@me.com>
2023-07-15 12:31:30 -0400

    Animation tutorial 2 complete.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,16 +13,45 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 0.25
+            //animationAmount += 0.75
             
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3 )
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 2)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+        //.scaleEffect(animationAmount)
+        //.blur(radius: (animationAmount - 1) * 3 )
+        //.animation(.default, value: animationAmount)
+        //.animation(.easeIn, value: animationAmount)
+        //.animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+        //.animation(.easeInOut(duration: 2), value: animationAmount)
+        //.animation(
+        //    .easeInOut(duration: 2)
+        //    .delay(1),
+        //    value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2)
+//            .repeatCount(3, autoreverses: true),
+//            value: animationAmount)
+        
+//        .animation(
+//            .easeInOut(duration: 2)
+//            .repeatForever(autoreverses: true),
+//            value: animationAmount)
     }
 }
 
